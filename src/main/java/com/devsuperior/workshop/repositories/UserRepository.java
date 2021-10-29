348678a1521070200bb6bcb2f8ac7821e245e713
Leonardo <demetrio.vd@hotmail.com>
2021-10-28 23:07:02 -0300

    Service layer, component registration

--- src/main/java/com/devsuperior/workshop/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/workshop/repositories/UserRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.workshop.entities.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long>{
 }
