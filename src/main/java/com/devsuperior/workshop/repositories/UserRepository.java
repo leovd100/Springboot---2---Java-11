2cf74d5b295d74169658424719c1028bd43ce32b
Leonardo <demetrio.vd@hotmail.com>
2021-10-28 22:45:58 -0300

    JPA repository, dependency injection, database seending

---
+++ src/main/java/com/devsuperior/workshop/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.devsuperior.workshop.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.workshop.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+}
