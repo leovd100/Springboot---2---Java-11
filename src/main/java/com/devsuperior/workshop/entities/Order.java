2a5a194ca3924851215692d412ce7abe099d23cb
Leonardo <demetrio.vd@hotmail.com>
2021-11-04 10:55:44 -0300

    Oder, Instant, ISO 8601 - Part 1

---
+++ src/main/java/com/devsuperior/workshop/entities/Order.java
@@ -0,0 +1,90 @@
+package com.devsuperior.workshop.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_order")
+public class Order implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	private Instant instant;
+	
+	
+	@ManyToOne
+	@JoinColumn(name = "client_id")
+	private User client;
+	
+	
+	public Order() {
+	
+	}
+
+	public Order(Long id, Instant instant, User client) {
+		Id = id;
+		this.instant = instant;
+		this.client = client;
+	}
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public Instant getInstant() {
+		return instant;
+	}
+
+	public void setInstant(Instant instant) {
+		this.instant = instant;
+	}
+
+	public User getClient() {
+		return client;
+	}
+
+	public void setClient(User client) {
+		this.client = client;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(Id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		return Objects.equals(Id, other.Id);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
