7cab01fb49fe130d2489b0778e2f646ea3467c36
Leonardo <demetrio.vd@hotmail.com>
2021-11-06 13:27:26 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/devsuperior/workshop/repositories/OrderItemRepository.java
+++ src/main/java/com/devsuperior/workshop/repositories/OrderItemRepository.java
@@ -3,7 +3,6 @@ package com.devsuperior.workshop.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.workshop.entities.OrderItem;
-import com.devsuperior.workshop.entities.User;
 
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
