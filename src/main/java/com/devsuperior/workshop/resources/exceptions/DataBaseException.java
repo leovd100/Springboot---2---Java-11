669093127df4d41a69115a67156d7b4fe6342ee7
Leonardo <demetrio.vd@hotmail.com>
2021-11-09 23:30:11 -0300

    Exception handling - delete

---
+++ src/main/java/com/devsuperior/workshop/resources/exceptions/DataBaseException.java
@@ -0,0 +1,11 @@
+package com.devsuperior.workshop.resources.exceptions;
+
+public class DataBaseException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public DataBaseException(String msg) {
+		super(msg);
+	}
+	
+}
--- src/main/java/com/devsuperior/workshop/services/UserService.java
+++ src/main/java/com/devsuperior/workshop/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.workshop.entities.User;
 import com.devsuperior.workshop.repositories.UserRepository;
+import com.devsuperior.workshop.resources.exceptions.DataBaseException;
 import com.devsuperior.workshop.services.exceptions.ResourceNotFoundExcpetion;
 
 
@@ -31,7 +34,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundExcpetion(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
